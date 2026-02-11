37d83490adad43222e88a8b90bf9435a7f89a402
Thomas Spader <thomas.spader@gmail.com>
2026-02-11 17:51:45 -0500

    chore: formatting

---
+++ tools/format.ts
@@ -0,0 +1,38 @@
+import path from "path";
+import { $ } from "bun";
+
+export namespace Format {
+  const paths = {
+    root: path.join(import.meta.dir, ".."),
+    child: (relative: string) => path.join(paths.root, relative),
+  };
+
+  export const clang = async () => {
+    const glob = new Bun.Glob("plugins/imessage/src/**/*.{c,h}");
+    const files = [] as string[];
+
+    for (const file of glob.scanSync({ cwd: paths.root, onlyFiles: true })) {
+      files.push(paths.child(file));
+    }
+
+    if (files.length === 0) {
+      return;
+    }
+
+    await $`clang-format -i --style=file ${files}`;
+  };
+
+  export const swift = async () => {
+    await $`swift format --configuration ${paths.child(".swift-format")} --in-place --recursive ${paths.child("source/ios")}`;
+  };
+
+  export const ts = async () => {
+    await $`prettier --write ${paths.child("source/**/*.{ts,tsx}")} ${paths.child("*.{json,md}")}`;
+  };
+}
+
+if (import.meta.main) {
+  await Format.clang();
+  await Format.swift();
+  await Format.ts();
+}
